45a8f539f9a748080394fea28ac405702d8887cb
GrigoryKovalev <grigorykov@gmail.com>
2022-07-21 01:02:04 +0300

    updated

--- src/Pagination/Paginator.php
+++ src/Pagination/Paginator.php
@@ -3,6 +3,9 @@
 namespace Laravelayers\Pagination;
 
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\UrlWindow;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use Laravelayers\Contracts\Foundation\Dto\Dtoable;
 
 class Paginator extends LengthAwarePaginator implements Dtoable
@@ -22,4 +25,35 @@ class Paginator extends LengthAwarePaginator implements Dtoable
      * @var string
      */
     public static $defaultSummaryView = 'pagination::summary';
+
+    /**
+     * Indicates whether to use the first page number in the URL.
+     *
+     * @var string
+     */
+    public static $isFirstPage = false;
+
+    /**
+     * Get the URL for a given page number.
+     *
+     * @param  int  $page
+     * @return string
+     */
+    public function url($page)
+    {
+        if ($page > 1 || static::$isFirstPage) {
+            return parent::url($page);
+        }
+
+        $url = $this->path();
+
+        $parameters = Arr::except($this->query, 'page');
+
+        if ($parameters) {
+            $url .= (Str::contains($this->path(), '?') ? '&' : '?')
+                . Arr::query($parameters);
+        }
+
+        return $url . $this->buildFragment();
+    }
 }
